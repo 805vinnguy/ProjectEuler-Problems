74fddd364c6f6d5026c11c0e41bca17653685e7c
Vinh Nguyen <805vinnguy@gmail.com>
2018-08-24 22:13:22 -0700

    initial commit

---
+++ 3_LargestPrimeFactor/Solution2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Solution2 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
+        /**
+            consider using either quadratic sieve or gnfs
+            quadratic is simpler
+            rename solution to whatever you decide
+         */
+    }
+}
